6dc178ddf51aafc217e315976232b598c6978fd0
nacl <uhohnaomi@gmail.com>
2019-11-15 17:11:50 -0800

    First draft of raw streaming PCM interface

--- sound.py
+++ sound.py
@@ -3,9 +3,12 @@ import cpu
 
 from array import array
 from time import sleep
+import struct
+import io
+import wave
 
 import pygame
-from pygame.mixer import Sound, get_init, pre_init
+from pygame.mixer import Sound, music, get_init, pre_init
 
 class SOUND(object):
     def __init__(self, bus):
@@ -62,7 +65,6 @@ class SOUND(object):
 
     # TODO: reset()
 
-
 class Note(Sound):
     # TODO: actually do this stuff
     def __init__(self, frequency, volume=.1):
@@ -74,17 +76,80 @@ class Note(Sound):
         period = int(round(get_init()[0] / self.frequency))
         samples = array("h", [0] * period)
         amplitude = 2 ** (abs(get_init()[1]) - 1) - 1
-        for time in xrange(period):
+        for time in range(period):
             if time < period / 2:
                 samples[time] = amplitude
             else:
                 samples[time] = -amplitude
         return samples
 
+class RawPCMStream(object):
+    def __init__(self, sample_rate = 44100, stereo = True, buffer_seconds = 1):
+        self.n_channels = {True: 2, False: 1}[stereo]
+        self.sample_rate = sample_rate
+        self.sample_bytes = 2
+        self.buffer_seconds = buffer_seconds
 
+        self.riff_buffer = io.BytesIO()
+
+        w = wave.open(self.riff_buffer, "wb")
+        w.setsampwidth(self.sample_bytes)
+        w.setnchannels(self.n_channels)
+        w.setframerate(self.sample_rate)
+        sentinel_value = b"\xDA\x7A"
+        w.writeframes(sentinel_value * int(self.sample_bytes * self.sample_rate * self.buffer_seconds / len(sentinel_value)))
+        w.close()
+        self.riff_buffer.seek(0)
+
+        self.riff_data_offset = self.riff_buffer.getvalue().index(sentinel_value)
+
+        self.data_buffer = self.riff_buffer.getbuffer()[self.riff_data_offset:].cast('h')
+
+    def sample_buffer(self):
+        return self.data_buffer
+
+    def tell(self, *args, **kwargs):
+        return self.riff_buffer.tell(*args, **kwargs)
+
+    def read(self, *args, **kwargs):
+        return self.riff_buffer.read(*args, **kwargs)
+
+    def seek(self, *args, **kwargs):
+        return self.riff_buffer.seek(*args, **kwargs)
+
+    def connect(self):
+        pygame.mixer.music.load(self)
+        pygame.mixer.music.play(loops=-1)
+
+
+def wave_data(freq, n_seconds, data_buffer):
+    period = int(round(get_init()[0] / freq))
+    n_samples = get_init()[0] * n_seconds
+    amplitude = 2 ** (abs(get_init()[1]) - 1) - 1
+    for time in range(n_samples):
+        if (time % period) < period / 2:
+            data_buffer[time] = amplitude
+        else:
+            data_buffer[time] = -amplitude
 
 if __name__ == "__main__":
     pre_init(44100, -16, 1, 1024)
     pygame.init()
-    Note(440,.1).play(-1)
-    sleep(5)
+    # Note(440,.001).play(-1)
+    # pygame.mixer.music.load(GameboyMixerStream(44100, 440))
+
+    pcm_stream = RawPCMStream(sample_rate = get_init()[0], stereo = False)
+
+    wave_data(440, pcm_stream.buffer_seconds, pcm_stream.sample_buffer())
+
+    pygame.mixer.music.set_volume(0.05)
+    pcm_stream.connect()
+
+    for _ in range(10):
+        sleep(.04)
+        wave_data(640, pcm_stream.buffer_seconds, pcm_stream.sample_buffer())
+        sleep(.04)
+        wave_data(440, pcm_stream.buffer_seconds, pcm_stream.sample_buffer())
+    sleep(.2)
+
+
